37f747343b85fbb50a638113e9bd01e4a11d336c
danjoseph bonao <magicofdanjosephbonao@gmail.com>
2024-07-18 13:42:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Dan Joseph Bonao</h1>
+<h2>33-ITE-03</h2>
+<h3>3rd Year Summer Semester</h3>
+</body>
+</html>
 
-kjfjbnejbfejbfekfjbfj
-idok patrick
-patrick balkfneBNFJEJefe
